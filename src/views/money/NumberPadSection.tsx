2a5e6def294c92589628b3bea363a7d78fc2cb0a
ylm3112370004 <wintersweety@126.com>
2020-09-01 20:59:40 +0800

    数字面板实现

--- src/views/money/NumberPadSection.tsx
+++ src/views/money/NumberPadSection.tsx
@@ -1,6 +1,7 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const NumberPadSection = styled.section`
+const Wrapper = styled.section`
 > .output {
   background: #fff;
   font-size: 36px;
@@ -58,6 +59,78 @@ const NumberPadSection = styled.section`
   }
 }
 `
+
+const NumberPadSection: React.FC = () => {
+  const [output, setOutput] = useState<string>('0')
+  const _setOutput = (output: string) => {
+    if(output.length > 16) {
+      output = output.slice(0, 16)
+    } else if(output.length === 0) {
+      output = '0'
+    }
+    setOutput(output)
+  }
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent
+    if(text === null) { return }
+    if(text) {
+      switch(text) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+          if(output === "0") {
+            _setOutput(text)
+          } else {
+            _setOutput(output + text)
+          }
+          break;
+        case '.':
+          if(output.indexOf(".") >= 0) { break }
+          _setOutput(output + ".")
+          break;
+        case '清除':
+          if(output.length === 1) {
+            _setOutput("")
+          } else {
+            _setOutput(output.slice(0, -1))
+          }
+          break;
+        case '清空':
+          _setOutput("")
+          break;
+        case 'ok':
+          console.log('确认', output)
+          break;
+      }
+    }
+  }
+  return <Wrapper>
+    <div className="output">{output}</div>
+        <div className="pad clearfix" onClick={onClickButtonWrapper}>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>清除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className="ok">ok</button>
+          <button className="zero">0</button>
+          <button className="dot">.</button>
+        </div>
+  </Wrapper>
+}
 
 export {
   NumberPadSection
